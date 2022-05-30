29b9bfe6d7041a4fe23582fd3fed730471d9a0c2
jakub-mrow <jakub@mrowczynski.net>
2022-05-30 19:54:27 +0200

    added progress bar

--- app/src/main/java/com/example/boardgamecollector/ProfileFragment.kt
+++ app/src/main/java/com/example/boardgamecollector/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.example.boardgamecollector
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,27 +8,48 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.boardgamecollector.databinding.FragmentProfileBinding
+import java.time.LocalDateTime
 
 class ProfileFragment : Fragment() {
     private lateinit var binding: FragmentProfileBinding
     private lateinit var dbHandler: DBHandler
 
+    @SuppressLint("SetTextI18n")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentProfileBinding.inflate(inflater, container, false)
+
+        binding.profileNameTxt.text = dbHandler.getName().toString()
+        binding.gamesCollectionText.text = "Games collection: " + dbHandler.countGames().toString()
+        binding.dlcCollectionText.text = "DLC collection: " + dbHandler.countDLC().toString()
+        binding.lastSyncText.text = "Last sync: "+ "\n" + convertDate(LocalDateTime.now())
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.exitBtn.setOnClickListener {
+            dbHandler.deleteUsers()
+            dbHandler.deleteBoardGames()
+            dbHandler.deleteDLC()
+        }
     }
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
         dbHandler = DBHandler(context, null)
 
+    }
+
+    fun convertDate(date: LocalDateTime): String {
+        val datePart = date.toString().split("T")[0]
+        val timePart = date.toString().split("T")[1]
+        val timeWithoutMiliSeconds = timePart.split(":").slice(0..1).joinToString(":")
+        //return date.toString().split("T").joinToString(" ")
+        return "$datePart $timeWithoutMiliSeconds"
     }
 
 }
