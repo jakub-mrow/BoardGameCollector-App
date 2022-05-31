701179c3c75b0fc16ae7234b26f94ea2b02fd112
jakub-mrow <jakub@mrowczynski.net>
2022-05-31 20:44:16 +0200

    implemented images to show on list of board games

--- app/src/main/java/com/example/boardgamecollector/BoardGamesFragment.kt
+++ app/src/main/java/com/example/boardgamecollector/BoardGamesFragment.kt
@@ -7,10 +7,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.SimpleCursorAdapter
 import android.widget.TextView
 import androidx.navigation.fragment.findNavController
 import com.example.boardgamecollector.databinding.FragmentBoardGamesBinding
+import com.squareup.picasso.Picasso
 
 class BoardGamesFragment : Fragment() {
     private lateinit var binding: FragmentBoardGamesBinding
@@ -43,14 +45,21 @@ class BoardGamesFragment : Fragment() {
     }
 
     private fun setAdapter(context: Context, cursor: Cursor) {
-        val columns = arrayOf("_id", "title", "release_date", "rank")
-        val id = intArrayOf(R.id.gameId, R.id.gameTitle, R.id.gameYear, R.id.gameRank)
+        val columns = arrayOf("_id", "title", "release_date", "rank", "image")
+        val id = intArrayOf(R.id.gameId, R.id.gameTitle, R.id.gameYear, R.id.gameRank, R.id.gameImage)
         adapter = SimpleCursorAdapter(context, R.layout.game_boards_view_template, cursor, columns, id, 0)
         adapter.setViewBinder { view, dbCursor, column ->
             when (view.id) {
                 R.id.gameId, R.id.gameTitle, R.id.gameYear, R.id.gameRank -> {
                     val textView = view as TextView
                     textView.text = dbCursor.getString(column)
+                }
+                R.id.gameImage -> {
+                    val imageUrl = dbCursor.getString(column)
+                    val imageView = view as ImageView
+                    if (imageUrl != null){
+                        Picasso.get().load(imageUrl).into(imageView)
+                    }
                 }
             }
             return@setViewBinder true
